74b9454a8d5e9217bad53a6f0a2fcc3f345888b7
Domenico Sarcina <50576909+domenicosarcina@users.noreply.github.com>
2025-07-14 11:30:48 +0200

    Fix Navbar href

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,7 +15,7 @@ export default function StickyNavbar() {
     <div className="sticky top-0 z-50 bg-white shadow-md dark:bg-gray-800 transition-colors duration-300">
       <nav className="w-full max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
         <div className="text-xl font-semibold text-blue-600 dark:text-blue-400 flex items-center gap-2">
-          <a href="#home" className="hover:text-blue-500">
+          <a href="/#home" className="hover:text-blue-500">
             DS
           </a>
           <Weather />
@@ -40,27 +40,27 @@ export default function StickyNavbar() {
         </div>
         <ul className="hidden md:flex gap-6 text-gray-700 font-bold dark:text-gray-300 items-center">
           <li>
-            <a href="#home" className="hover:text-blue-500">
+            <a href="/#home" className="hover:text-blue-500">
               Home
             </a>
           </li>
           <li>
-            <a href="#skills" className="hover:text-blue-500">
+            <a href="/#skills" className="hover:text-blue-500">
               {t("skillsMenu")}
             </a>
           </li>
           <li>
-            <a href="#work" className="hover:text-blue-500">
+            <a href="/#work" className="hover:text-blue-500">
               {t("jobsMenu")}
             </a>
           </li>
           <li>
-            <a href="#study" className="hover:text-blue-500">
+            <a href="/#study" className="hover:text-blue-500">
               {t("studiesMenu")}
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-blue-500">
+            <a href="/#contact" className="hover:text-blue-500">
               {t("contactMenu")}
             </a>
           </li>
@@ -89,7 +89,7 @@ export default function StickyNavbar() {
         >
           <li>
             <a
-              href="#home"
+              href="/#home"
               className="hover:text-blue-500"
               onClick={() => setOpen(false)}
             >
@@ -98,7 +98,7 @@ export default function StickyNavbar() {
           </li>
           <li>
             <a
-              href="#skills"
+              href="/#skills"
               className="hover:text-blue-500"
               onClick={() => setOpen(false)}
             >
@@ -107,7 +107,7 @@ export default function StickyNavbar() {
           </li>
           <li>
             <a
-              href="#work"
+              href="/#work"
               className="hover:text-blue-500"
               onClick={() => setOpen(false)}
             >
@@ -116,7 +116,7 @@ export default function StickyNavbar() {
           </li>
           <li>
             <a
-              href="#study"
+              href="/#study"
               className="hover:text-blue-500"
               onClick={() => setOpen(false)}
             >
@@ -125,7 +125,7 @@ export default function StickyNavbar() {
           </li>
           <li>
             <a
-              href="#contact"
+              href="/#contact"
               className="hover:text-blue-500"
               onClick={() => setOpen(false)}
             >
