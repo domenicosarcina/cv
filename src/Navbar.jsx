3ae8f2e2574272f6f01b1990d31f4826bff1a95d
Domenico Sarcina <50576909+domenicosarcina@users.noreply.github.com>
2025-07-14 15:44:41 +0200

    Update NavBar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,12 +5,21 @@ import { Moon, Sun } from "lucide-react";
 import { LanguageSwitcher } from "./components/ui/LanguageSwithcer";
 import { useTranslation } from "react-i18next";
 import Weather from "./components/ui/Weather";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 
 export default function StickyNavbar() {
   const [open, setOpen] = useState(false);
   const { darkMode, toggleDarkMode } = useThemeStore();
   const { t } = useTranslation();
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="sticky top-0 z-50 bg-white shadow-md dark:bg-gray-800 transition-colors duration-300">
@@ -20,18 +29,19 @@ export default function StickyNavbar() {
             to="/"
             className="hover:text-blue-500"
             onClick={(e) => {
-              e.preventDefault();
-              document
-                .getElementById("home")
-                ?.scrollIntoView({ behavior: "smooth" });
+              if (isHomePage) {
+                e.preventDefault();
+                handleScroll("home");
+              }
             }}
           >
             DS
           </Link>
           <Weather />
         </div>
+
         <div className="md:hidden flex items-center gap-4">
-          <button onClick={toggleDarkMode} className="md:hidden">
+          <button onClick={toggleDarkMode}>
             {darkMode ? (
               <Sun className="text-blue-400" size={18} />
             ) : (
@@ -39,88 +49,84 @@ export default function StickyNavbar() {
             )}
           </button>
           <LanguageSwitcher />
-
           <button
-            className="md:hidden text-3xl text-blue-600 focus:outline-none dark:text-blue-400 hover:text-blue-500"
+            className="text-3xl text-blue-600 dark:text-blue-400 hover:text-blue-500"
             onClick={() => setOpen(!open)}
             aria-label="Toggle menu"
           >
             <GiHamburgerMenu />
           </button>
         </div>
+
         <ul className="hidden md:flex gap-6 text-gray-700 font-bold dark:text-gray-300 items-center">
           <li>
             <Link
               to="/"
               className="hover:text-blue-500"
               onClick={(e) => {
-                e.preventDefault();
-                document
-                  .getElementById("home")
-                  ?.scrollIntoView({ behavior: "smooth" });
+                if (isHomePage) {
+                  e.preventDefault();
+                  handleScroll("home");
+                }
               }}
             >
               Home
             </Link>
           </li>
+          {isHomePage && (
+            <>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleScroll("skills");
+                  }}
+                >
+                  {t("skillsMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleScroll("work");
+                  }}
+                >
+                  {t("jobsMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleScroll("study");
+                  }}
+                >
+                  {t("studiesMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleScroll("contact");
+                  }}
+                >
+                  {t("contactMenu")}
+                </Link>
+              </li>
+            </>
+          )}
           <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault();
-                document
-                  .getElementById("skills")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("skillsMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault(); // evita il cambio pagina
-                document
-                  .getElementById("work")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("jobsMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault();
-                document
-                  .getElementById("study")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("studiesMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault(); // evita il cambio pagina
-                document
-                  .getElementById("contact")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("contactMenu")}
-            </Link>
-          </li>
-          <li className="flex items-center">
-            <button onClick={toggleDarkMode} className="">
+            <button onClick={toggleDarkMode}>
               {darkMode ? <Sun size={18} /> : <Moon size={18} />}
             </button>
           </li>
@@ -129,94 +135,83 @@ export default function StickyNavbar() {
           </li>
         </ul>
       </nav>
+
       {open && (
         <ul
           className={`md:hidden flex flex-col gap-4 px-8 pb-4 pt-4 text-gray-700 font-bold bg-white shadow
-            absolute left-0 top-[64px] w-full dark:bg-gray-800 dark:text-gray-300
-            transition-all duration-300
-            ${
-              open
-                ? "opacity-100 translate-y-0"
-                : "opacity-0 -translate-y-4 pointer-events-none"
-            }
-          `}
-          style={{ willChange: "opacity, transform" }}
+          absolute left-0 top-[64px] w-full dark:bg-gray-800 dark:text-gray-300 transition-all duration-300`}
         >
           <li>
             <Link
               to="/"
               className="hover:text-blue-500"
               onClick={(e) => {
-                e.preventDefault();
+                if (isHomePage) {
+                  e.preventDefault();
+                  handleScroll("home");
+                }
                 setOpen(false);
-                document
-                  .getElementById("home")
-                  ?.scrollIntoView({ behavior: "smooth" });
               }}
             >
               Home
             </Link>
           </li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault();
-                setOpen(false);
-                document
-                  .getElementById("skills")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("skillsMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/ork"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault();
-                setOpen(false);
-                document
-                  .getElementById("work")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("jobsMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/#study"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault();
-                setOpen(false);
-                document
-                  .getElementById("study")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("studiesMenu")}
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="/"
-              className="hover:text-blue-500"
-              onClick={(e) => {
-                e.preventDefault(); // evita il cambio pagina
-                setOpen(false);
-                document
-                  .getElementById("contact")
-                  ?.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              {t("contactMenu")}
-            </Link>
-          </li>
+          {isHomePage && (
+            <>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                    handleScroll("skills");
+                  }}
+                >
+                  {t("skillsMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                    handleScroll("work");
+                  }}
+                >
+                  {t("jobsMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                    handleScroll("study");
+                  }}
+                >
+                  {t("studiesMenu")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="hover:text-blue-500"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                    handleScroll("contact");
+                  }}
+                >
+                  {t("contactMenu")}
+                </Link>
+              </li>
+            </>
+          )}
           <li>
             <Weather mobile={true} />
           </li>
